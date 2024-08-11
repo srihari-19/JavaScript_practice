eabed2367ceeae63b22feadbb66fc7ba2ce21885
srihari-19 <sriharisathya15@gmail.com>
2024-08-11 13:12:19 +0530

    new array
    new array update

--- script.js
+++ script.js
@@ -4,10 +4,41 @@
 // computer.unshift("hai ", 45)
 // console.log(computer)
 
-let computers = ["Desktop", "Laptop", "Tablet PC"];
+// let computers = ["Desktop", "Laptop", "Tablet PC"];
 
-computers.forEach(function(item){
-    item = `<li>${item}</li>`
-    console.log(item)
-})
-console.log(computers)
+// computers.forEach(function(item){
+//     item = `<li>${item}</li>`
+//     console.log(item)
+// })
+// console.log(computers)
+const my_desktop =[
+    "laptop",
+    "mouse",
+    "Keyboard",
+    'CPU',
+    "USB",
+    "Headphone"
+];
+console.log("Main Item: ", my_desktop);
+// console.log("removed Item: ", my_desktop.pop());
+// console.log("New list: ", my_desktop)
+// let remove = my_desktop.pop();
+// console.log("Removed item was: ",remove)
+//shit to last item 2 first
+// my_desktop.unshift(remove)
+// console.log("last item has beeen shifted: ",my_desktop);
+// my_desktop.sort();
+// console.log("array sorted Alphabetically: ",my_desktop)
+
+// for find something in array
+// let find = "USB";
+// const item_found = my_desktop.find((something) => something === find )|| "NO such item";
+// console.log("Search Item has found: ",item_found)
+
+//Remove specific item from the array
+// let remove_item = "Keyboard";
+// my_desktop.splice(my_desktop.indexOf(remove_item),1);
+// console.log(`Array item with name "${remove_item}" has been removed:`, my_desktop)
+
+// my_desktop.splice(2,3)
+// console.log(my_desktop)
