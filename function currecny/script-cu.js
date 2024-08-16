081a8805afec7b6d744f286e3cd64125d998e964
srihari-19 <sriharisathya15@gmail.com>
2024-08-16 10:34:10 +0530

    Function
    Java-script build in function

--- function currecny/script-cu.js
+++ function currecny/script-cu.js
@@ -1,26 +1,49 @@
-const taxCalculator = (sum, percentage, currency, prefix) => {
+// const taxCalculator = (sum, percentage, currency, prefix) => {
 
-    let tax = sum * (percentage / 100);
-    let total = sum + tax;
-    if (prefix) {
+//     let tax = sum * (percentage / 100);
+//     let total = sum + tax;
+//     if (prefix) {
+//         console.log(`
+//             sum before tax: ${currency} ${sum}
+//             tax percentage: ${percentage}%
+//             tax     :       ${currency} ${tax.toFixed(2)}
+//             Total :         ${currency} ${total.toFixed(2)}
+//             `)
+
+//     }
+//     else {
+//         console.log(`
+//             sum before tax:  ${sum}${currency}
+//             tax percentage: ${percentage}%
+//             tax     :        ${tax.toFixed(2)}${currency}
+//             Total :          ${total.toFixed(2)}${currency}
+//             `)
+//     }
+
+// };
+
+// // €
+// taxCalculator(99, 15, "rupees",false);
+
+
+const formatter = (local,currency,value)=>{
+    let formatterValue = new Intl.NumberFormat(local,{
+        style:"currency",
+        currency:currency,
+    }).format(value);
+    return formatterValue;
+}
+
+const taxCalculator = (sum, percentage,local,currency,) => {
+
+        let tax = sum * (percentage / 100);
+        let total = sum + tax;
         console.log(`
-            sum before tax: ${currency} ${sum}
-            tax percentage: ${percentage}%
-            tax     :       ${currency} ${tax.toFixed(2)}
-            Total :         ${currency} ${total.toFixed(2)}
-            `)
-
-    }
-    else {
-        console.log(`
-            sum before tax:  ${sum}${currency}
-            tax percentage: ${percentage}%
-            tax     :        ${tax.toFixed(2)}${currency}
-            Total :          ${total.toFixed(2)}${currency}
-            `)
+                    sum before tax: ${formatter(local,currency,sum)}
+                    tax percentage: ${percentage}%
+                    tax     :       ${formatter(local,currency,tax)}
+                    Total :         ${formatter(local,currency,total)}
+                    `)
     }
 
-};
-
-// €
-taxCalculator(99, 15, "rupees",false);
+    taxCalculator(99, 15,"en-IN","INR");
