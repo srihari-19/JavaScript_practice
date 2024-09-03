05d5c6304ec101cf599fbc6ea8849117db19829c
srihari-19 <sriharisathya15@gmail.com>
2024-09-03 16:54:31 +0530

    update

--- function currecny/script-cu.js
+++ function currecny/script-cu.js
@@ -26,24 +26,50 @@
 // taxCalculator(99, 15, "rupees",false);
 
 
-const formatter = (local,currency,value)=>{
-    let formatterValue = new Intl.NumberFormat(local,{
-        style:"currency",
-        currency:currency,
+const formatter = (local, currency, value) => {
+    let formatterValue = new Intl.NumberFormat(local, {
+        style: "currency",
+        currency: currency,
     }).format(value);
     return formatterValue;
 }
 
-const taxCalculator = (sum, percentage,local,currency,) => {
+//  for html output
+const taxprint = (tableValue) => {
+    const addTable = document.createElement("table");
+    addTable.innerHTML = `
+        <tr>
+            <td>sum before tax</td>
+            <td>${tableValue.sum}</td>
+        </tr>
+         <tr>
+            <td> tax percentage</td>
+            <td>${tableValue.percentage}</td>
+        </tr>
+        <tr>
+            <td>rate of the tax</td>
+            <td>${tableValue.tax}</td>
+        </tr>
+        <tr>
+            <td>Total tax pluse amount </td>
+            <td>${tableValue.total}</td>
+        </tr>
+    `;
+    document.querySelector("main").append(addTable)
+}
 
-        let tax = sum * (percentage / 100);
-        let total = sum + tax;
-        console.log(`
-                    sum before tax: ${formatter(local,currency,sum)}
-                    tax percentage: ${percentage}%
-                    tax     :       ${formatter(local,currency,tax)}
-                    Total :         ${formatter(local,currency,total)}
-                    `)
+const taxCalculator = (sum, percentage, local, currency,callback) => {
+
+    let tax = sum * (percentage / 100);
+    let total = sum + tax;
+
+    const tableValue = {
+        sum     : formatter(local, currency, sum),
+        percentage : percentage,
+        tax : formatter(local, currency, tax),
+        total : formatter(local, currency, total)
     }
+    callback(tableValue);
+}
 
-    taxCalculator(99, 15,"en-IN","INR");
+taxCalculator(99, 15, "en-IN", "INR",taxprint );
