c297e00d2fe74f5b4b4cc5de0287b660dd8e9d9e
srihari-19 <sriharisathya15@gmail.com>
2024-09-03 20:21:23 +0530

    switch
    Using switch statement

--- webPageModernJS/script.js
+++ webPageModernJS/script.js
@@ -2,7 +2,7 @@ import computer from './computer.js';
 const laptop = new computer(
     "Lenova",
     "Intel Core i5",
-    "16GB",
+    16,
     "1TB",
     "Windows 11",
     true,
@@ -19,7 +19,7 @@ const content = `
 <h1 class="computer_name">${laptop.name}</h1>
 <ul class="computer_feature">
   <li class="feature processor">${laptop.processor}</li>
-  <li class="feature ram">${laptop.ram}</li>
+  <li class="feature ram">${laptop.ram}GB</li>
   <li class="feature storage">${laptop.storage}GB</li>
   <li class="feature window">${laptop.os}</li>
   <li class="feature wifi">${laptop.wifi ?"wifi available" : "wifi not available" }</li>
@@ -43,3 +43,19 @@ if (laptop.wifi){
 }else{
     console.log("wifi not available")
 }
+
+var checkram = ()=>{
+    switch(true){
+        case laptop.ram < 16:
+            console.log("Laptop has less than 16GB RAM")
+            break
+        case laptop.ram === 16:
+            console.log("Laptop has 16GB RAM")
+            break
+        case laptop.ram >=32:
+            console.log("laptop has 32GB RAM or Heigher")
+        default:
+            console.log("The configuration is mismatch please check the admin")
+    }
+}
+checkram();
