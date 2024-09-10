ba1bacead53b35ff444dc28d4a8a8a89b2f30471
srihari-19 <sriharisathya15@gmail.com>
2024-09-10 23:42:55 +0530

    For and while loop
    nested loop paractice

--- Practice_chapter/script.js
+++ Practice_chapter/script.js
@@ -1,17 +1,38 @@
-function isEven(userInput, numUser){
-    let count = 0;
-    for (let index = 0; index < userInput.length; index++) {
-      if (userInput[index]===numUser){
-        count++
-        
-      }
-        
-    }
-    return count
+student = [
+    { id: 1, name: "sam" },
+    { id: 2, name: 'nova' },
+    { id: 3, name: 'john' },
+    { id: 4, name: 'lila' },
+    { id: 5, name: 'meena' },
+];
 
+schoolA_ID = [1, 3, 5];
+schoolB_ID = [2, 4];
+
+// For nested loop 
+function printName(Ids) {
+    result = [];
+    // first need to iterate the school function
+    let schoolIteration = 0;
+    while (schoolIteration < Ids.length) {
+        const studentsIds = Ids[schoolIteration]
+        // console.log(studentsIds)
+        // once first run the while loop the nested loop will start iterat
+
+        for (let studentIterator = 0; studentIterator < student.length; studentIterator++) {
+            //  for nested loop to get the student array loop
+            const studentsMatch = student[studentIterator]
+            // console.log(studentsMatch)
+            if (studentsIds === studentsMatch.id) {
+                // console.log(studentsMatch.name)
+                result.push(studentsMatch.name)
+                
+            }
+        }
+        schoolIteration++
+    }
+    return result;
 }
 
-userValue = isEven("hello", "h");
-console.log(userValue)
-
-let book = "book";
+const studentNameOutput = printName(schoolA_ID);
+console.log(studentNameOutput)
