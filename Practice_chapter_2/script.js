dcfa2b83bebd5e6f0402748b9b1dbba141b38dd5
srihari-19 <sriharisathya15@gmail.com>
2024-09-12 10:43:56 +0530

    spread and rest
    spread and rest parameter

--- Practice_chapter_2/script.js
+++ Practice_chapter_2/script.js
@@ -1,12 +1,44 @@
-let i  =0;
-while (i<5) {
-    console.log(i)
-    i++
+// spread rest parameter
+let firstArray=["hai", "hello", "bye"];
+let secondArray = ["ram","tom","sam"];
+
+// let arrayMerg = firstArray.concat(secondArray);
+nameArray = [...firstArray,...secondArray]
+// console.log(nameArray);
+
+// for object
+let areaOnePerson = {
+    name1 : 'ram',
+    age1: 26,
+    gender1:"male"
 }
 
-do {
-    console.log(i)
-    i++
-} while (i<5);
-// let userInput = alert('hello')
-// console.log(userInput)
+let areaTwoPerson = {
+    name : 'sam',
+    age: 22,
+    gender:"male"
+}
+const mergePerson ={
+    ...areaOnePerson,
+    ...areaTwoPerson
+}
+// console.log(mergePerson)
+// useing strings 
+let studentName = "jimjam";
+let ans = [...studentName];
+// console.log(ans);
+
+
+// rest parameter
+
+const input = [1,2,3,4,5,6,7] 
+
+function findValue(...vvv){
+    let answer = 0;
+    for(let i=0;i<vvv.length;i++){
+        answer = answer + vvv[i]
+   
+    }
+    console.log(answer)
+}
+findValue(...input)
