b7cdfe26728b898a4ce53f095f3d30061d90ef45
srihari-19 <sriharisathya15@gmail.com>
2024-09-12 22:34:26 +0530

    object de structure
    object de structure

--- Practice_chapter_2/script.js
+++ Practice_chapter_2/script.js
@@ -1,44 +1,17 @@
-// spread rest parameter
-let firstArray=["hai", "hello", "bye"];
-let secondArray = ["ram","tom","sam"];
+// object destruture
+let employee ={
+    companyName:{
+        nameC:'zzz'
+    },
+    firstName:"john",
+    lastName:"pandiyan",
+    age:55,
+    healthcondition:"good"
+};
 
-// let arrayMerg = firstArray.concat(secondArray);
-nameArray = [...firstArray,...secondArray]
-// console.log(nameArray);
+let {firstName} = employee;
 
-// for object
-let areaOnePerson = {
-    name1 : 'ram',
-    age1: 26,
-    gender1:"male"
-}
-
-let areaTwoPerson = {
-    name : 'sam',
-    age: 22,
-    gender:"male"
-}
-const mergePerson ={
-    ...areaOnePerson,
-    ...areaTwoPerson
-}
-// console.log(mergePerson)
-// useing strings 
-let studentName = "jimjam";
-let ans = [...studentName];
-// console.log(ans);
-
-
-// rest parameter
-
-const input = [1,2,3,4,5,6,7] 
-
-function findValue(...vvv){
-    let answer = 0;
-    for(let i=0;i<vvv.length;i++){
-        answer = answer + vvv[i]
-   
-    }
-    console.log(answer)
-}
-findValue(...input)
+let{companyName:{nameC}}=employee;
+console.log(firstName)
+console.log(employee.firstName)
+console.log(nameC)
