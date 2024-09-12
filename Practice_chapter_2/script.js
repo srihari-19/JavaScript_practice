c19f3e82f7b76f90553235c019b907ed375571ae
srihari-19 <sriharisathya15@gmail.com>
2024-09-12 22:52:19 +0530

    another method
    de structure

--- Practice_chapter_2/script.js
+++ Practice_chapter_2/script.js
@@ -9,9 +9,26 @@ let employee ={
     healthcondition:"good"
 };
 
-let {firstName} = employee;
+// let {firstName} = employee;
 
-let{companyName:{nameC}}=employee;
-console.log(firstName)
-console.log(employee.firstName)
-console.log(nameC)
+// let{companyName:{nameC}}=employee;
+// console.log(firstName)
+// console.log(employee.firstName)
+// console.log(nameC)
+
+
+function printName({firstName, age}) {
+    console.log(firstName+ "-" +age)
+}
+printName(employee)
+
+// array and object de structure new one
+function calculate(a,b){
+    let add = a+b;
+    let sub = a-b;
+    let mult = a*b;
+    return[add,sub,mult]
+}
+
+let final = calculate(10,10)
+
