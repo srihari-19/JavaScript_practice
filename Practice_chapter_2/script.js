84d700d498c53d9283a2ff68962a2bd66df2f481
srihari-19 <sriharisathya15@gmail.com>
2024-09-12 23:54:48 +0530

    try of
    try

--- Practice_chapter_2/script.js
+++ Practice_chapter_2/script.js
@@ -1,12 +1,12 @@
 // object destruture
-let employee ={
-    companyName:{
-        nameC:'zzz'
+let employee = {
+    companyName: {
+        nameC: 'zzz'
     },
-    firstName:"john",
-    lastName:"pandiyan",
-    age:55,
-    healthcondition:"good"
+    firstName: "john",
+    lastName: "pandiyan",
+    age: 55,
+    healthcondition: "good"
 };
 
 // let {firstName} = employee;
@@ -17,18 +17,51 @@ let employee ={
 // console.log(nameC)
 
 
-function printName({firstName, age}) {
-    console.log(firstName+ "-" +age)
+function printName({ firstName, age }) {
+    console.log(firstName + "-" + age)
 }
 printName(employee)
 
 // array and object de structure new one
-function calculate(a,b){
-    let add = a+b;
-    let sub = a-b;
-    let mult = a*b;
-    return[add,sub,mult]
+function calculate(a, b) {
+    let add = a + b;
+    let sub = a - b;
+    let mult = a * b;
+    return [add, sub, mult]
 }
 
-let final = calculate(10,10)
+let final = calculate(10, 10)
 
+//array & object destructure
+const numOfEmployee = {
+    company1: ['sam', 'ram', 'wan'],
+    company2: ['sela', 'mala', 'buula']
+}
+
+let { company1: [lada], company2: [, , jimm] } = numOfEmployee
+console.log(lada)
+console.log(jimm)
+
+
+// for array
+const trail = [
+    { name1: "don", age1: 15 },
+    { name2: "won", age2: 25 },
+]
+
+let [{age1:ring}] = trail;
+console.log(ring)
+
+let array111= ['hai','hello','bye'];
+console.log(...array111)
+function namePrint(ha){
+    console.log(ha)
+}
+let bvb = [1,2,3,4]
+let vbv = [11,22,33,44]
+
+printEmpoyee ={...employee}
+namePrint(printEmpoyee)
+console.log("normalprint",printEmpoyee)
+let mixArray = [...bvb,...vbv]
+console.log(mixArray)
