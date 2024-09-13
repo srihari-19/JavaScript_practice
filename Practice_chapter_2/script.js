917f47f42fd395e98577cdf2abd0c244d0920e1b
srihari-19 <sriharisathya15@gmail.com>
2024-09-13 11:16:43 +0530

    Deep&Shallow
    Deep&Shallow Method learning

--- Practice_chapter_2/script.js
+++ Practice_chapter_2/script.js
@@ -1,7 +1,8 @@
 // object destruture
 let employee = {
     companyName: {
-        nameC: 'zzz'
+        nameC: 'zzz',
+        YearOfStart:2020
     },
     firstName: "john",
     lastName: "pandiyan",
@@ -9,59 +10,19 @@ let employee = {
     healthcondition: "good"
 };
 
-// let {firstName} = employee;
-
-// let{companyName:{nameC}}=employee;
-// console.log(firstName)
-// console.log(employee.firstName)
-// console.log(nameC)
-
-
-function printName({ firstName, age }) {
-    console.log(firstName + "-" + age)
-}
-printName(employee)
-
-// array and object de structure new one
-function calculate(a, b) {
-    let add = a + b;
-    let sub = a - b;
-    let mult = a * b;
-    return [add, sub, mult]
-}
-
-let final = calculate(10, 10)
-
-//array & object destructure
-const numOfEmployee = {
-    company1: ['sam', 'ram', 'wan'],
-    company2: ['sela', 'mala', 'buula']
-}
-
-let { company1: [lada], company2: [, , jimm] } = numOfEmployee
-console.log(lada)
-console.log(jimm)
+// for Deep and shallow copy
+ let forCopy = JSON.parse(JSON.stringify(employee));
+ employee.firstName = "Pandiyan";
+ console.log(employee);
+ console.log(forCopy);
 
 
-// for array
-const trail = [
-    { name1: "don", age1: 15 },
-    { name2: "won", age2: 25 },
-]
 
-let [{age1:ring}] = trail;
-console.log(ring)
 
-let array111= ['hai','hello','bye'];
-console.log(...array111)
-function namePrint(ha){
-    console.log(ha)
-}
-let bvb = [1,2,3,4]
-let vbv = [11,22,33,44]
 
-printEmpoyee ={...employee}
-namePrint(printEmpoyee)
-console.log("normalprint",printEmpoyee)
-let mixArray = [...bvb,...vbv]
-console.log(mixArray)
+
+
+
+
+
+
