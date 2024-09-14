9befb69f156727c8697c68f17fd0b790de4a29e9
srihari-19 <sriharisathya15@gmail.com>
2024-09-14 13:03:55 +0530

    class manipulate
    class manipulate

--- Practice_chapter_2/script.js
+++ Practice_chapter_2/script.js
@@ -1,27 +1,34 @@
-// object destruture
-let employee = {
-    companyName: {
-        nameC: 'zzz',
-        YearOfStart:2020
-    },
-    firstName: "john",
-    lastName: "pandiyan",
-    age: 55,
-    healthcondition: "good"
-};
+let createlist1 = `
+    <li>Name</li>
+    <li>Number</li>
+    <li>Address</li>
+    <li>State</li>
+    <li>Distric</li>
+    <li>Pincode</li>
+    <button class="touch-btn">click</button>
+`;
+let mainMenu = document.querySelector("main");
+mainMenu.classList.add("mai-menu")
+let uList =document.createElement("ul");
+uList.classList.add("ul-class")
+uList.innerHTML = createlist1;
 
-// for Deep and shallow copy
- let forCopy = JSON.parse(JSON.stringify(employee));
- employee.firstName = "Pandiyan";
- console.log(employee);
- console.log(forCopy);
-
+mainMenu.append(uList);
 
 
 
-
-
-
+let changeBtn = document.querySelector(".touch-btn");
+changeBtn.addEventListener("click", (event)=>{
+    console.log(event)
+        // Toggle the background color (add/remove)
+        if (uList.style.backgroundColor === "yellow") {
+            uList.style.backgroundColor = "";  // Removes the background color
+            console.log('Background color removed');
+        } else {
+            uList.style.backgroundColor = "yellow";  // Adds the background color
+            console.log('Background color set to yellow');
+        }
+})
 
 
 
