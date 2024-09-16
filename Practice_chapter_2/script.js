039048280e2783e126f3075e1bb18f2475087847
srihari-19 <sriharisathya15@gmail.com>
2024-09-16 21:08:16 +0530

    classes

--- Practice_chapter_2/script.js
+++ Practice_chapter_2/script.js
@@ -1,35 +1,51 @@
-let createlist1 = `
-    <li>Name</li>
-    <li>Number</li>
-    <li>Address</li>
-    <li>State</li>
-    <li>Distric</li>
-    <li>Pincode</li>
-    <button class="touch-btn">click</button>
-`;
-let mainMenu = document.querySelector("main");
-mainMenu.classList.add("mai-menu")
-let uList =document.createElement("ul");
-uList.classList.add("ul-class")
-uList.innerHTML = createlist1;
+// let createlist1 = `
+//     <li>Name</li>
+//     <li>Number</li>
+//     <li>Address</li>
+//     <li>State</li>
+//     <li>Distric</li>
+//     <li>Pincode</li>
+//     <button class="touch-btn">click</button>
+// `;
+// let mainMenu = document.querySelector("main");
+// mainMenu.classList.add("mai-menu")
+// let uList =document.createElement("ul");
+// uList.classList.add("ul-class")
+// uList.innerHTML = createlist1;
 
-mainMenu.append(uList);
-
-
-
-let changeBtn = document.querySelector(".touch-btn");
-changeBtn.addEventListener("click", (event)=>{
-    console.log(event)
-        // Toggle the background color (add/remove)
-        if (uList.style.backgroundColor === "yellow") {
-            uList.style.backgroundColor = "";  // Removes the background color
-            console.log('Background color removed');
-        } else {
-            uList.style.backgroundColor = "yellow";  // Adds the background color
-            console.log('Background color set to yellow');
-        }
-})
+// mainMenu.append(uList);
 
 
+
+// let changeBtn = document.querySelector(".touch-btn");
+// changeBtn.addEventListener("click", (event)=>{
+//     console.log(event)
+//         // Toggle the background color (add/remove)
+//         if (uList.style.backgroundColor === "yellow") {
+//             uList.style.backgroundColor = "";  // Removes the background color
+//             console.log('Background color removed');
+//         } else {
+//             uList.style.backgroundColor = "yellow";  // Adds the background color
+//             console.log('Background color set to yellow');
+//         }
+// })
+
+
+// class get and set
+class Employee{
+    name1 = "kavin"
+    set employeeName(name){
+        console.log(this.employeeName1 = name)
+    }
+    
+    get employeerName(){
+        return this.name1;
+    }
+}
+
+let employee = new Employee()
+employee.employeeName = "ram"
+employee.employeerName = "kbd"
+console.log(employee.employeerName)
 
 
